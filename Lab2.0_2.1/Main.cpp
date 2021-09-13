a3628f863fa414d2d1d3ccc60a368f612b22bf56
fhctyw <lisnychukarsenw@gmail.com>
2021-09-13 21:43:04 +0300

    add another file

--- Lab2.0_2.1/Main.cpp
+++ Lab2.0_2.1/Main.cpp
@@ -17,17 +17,18 @@ int main()
 	double y;	//результат обчислення 1-го виразу
 	double y1;  //результат обчислення 2-го виразу
 
-	std::cout << "Введіть значення першої змінної(x): "; std::cin >> x;
+
+	std::cout << "Введіть значення першої змінної(x): "; std::cin >> x, x1;
 	std::cout << "Введіть значення другої змінної(x1): "; std::cin >> x1;
 
-	y = (std::sin(x) + std::cos(2 * x1 - x))		//		sin(a) + cos(2b - a)
+	y = (std::sin (x) + std::cos(2 * x1 - x))		//		sin(a) + cos(2b - a)
 						/							//		--------------------
 		(std::cos(x) - std::sin(2 * x1 - x));		//		cos(a) - sin(2b - a)
 
 	y1 = (1 + std::sin(2 * x1))			//		1 + sin(2b)
 				 /						//		-----------
 		  std::cos(2 * x1);				//		  cos(2b)
-
+	
 	std::cout << "Результат першого виразу: " << y << std::endl;
 	std::cout << "Результат другого виразу: " << y1 << std::endl;
 
